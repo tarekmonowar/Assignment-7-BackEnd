399757b6ae85b6e86ed63db3d478702719514354
Tarek Monowar <tarekmonowar353@gmail.com>
2026-04-22 01:22:21 +0600

    implemenst  ci/cd & ec2 setup

--- src/index.ts
+++ src/index.ts
@@ -2,7 +2,7 @@ import app from "./app.js"; // Ensure this import is correct
 import dotenv from "dotenv";
 dotenv.config();
 
-const port = process.env.PORT || 9000;
+const port = process.env.PORT || 9092;
 
 app.listen(port, () => {
   console.log(`Server is running on http://localhost:${port}`);
